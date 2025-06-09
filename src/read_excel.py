8f23e46e1a5ae71b5facb2f0f26075521bb96ef9
larrieu16 <larrieuestudos@gmail.com>
2025-06-09 09:20:20 -0300

    style: adds new libs and remove lines

--- src/read_excel.py
+++ src/read_excel.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 
+
 def read_excel_file(csv_path):
     spark = SparkSession.builder.appName("ImportExcelToDynamoDB").getOrCreate()
     return spark.read.csv(csv_path, header=True, inferSchema=True, sep=";")
